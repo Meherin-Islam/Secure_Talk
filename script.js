5fe508f70831b33a85db7cf752c6144d1b393936
Meherin-Islam <meherinislam59@gmail.com>
2024-12-18 21:46:49 +0600

    cypher done

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ class RSA {
         }
       }
     }
+
+    static generate(keysize) {
+        const e = bigInt(65537);
+        let p;
+        let q;
+        let totient;
+    
+        do {
+          p = this.randomPrime(keysize / 2);
+          q = this.randomPrime(keysize / 2);
+          totient = bigInt.lcm(p.prev(), q.prev());
+        } while (
+          bigInt.gcd(e, totient).notEquals(1) ||
+          p.minus(q).abs().shiftRight(keysize / 2 - 100).isZero()
+        );
